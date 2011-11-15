3263268b7e8a106a27ec21362ac76c95a0eb2535
Henri Korhola <henri.korhola@arcusys.fi>
2011-11-15 07:42:49 +0000

    Changed ws endpoints in logic.js for demo

--- hak/KOKU-HAK/Forms/Vahvistuspyynto_init_Form.gi/js/logic.js
+++ hak/KOKU-HAK/Forms/Vahvistuspyynto_init_Form.gi/js/logic.js
@@ -8,8 +8,8 @@ function intalioPreStart() {
 
 function getEndpoint() {
     
-    //var endpoint = "http://localhost:8180";
-    var endpoint = "http://trelx51x:8080";
+    var endpoint = "http://localhost:8180";
+    //var endpoint = "http://trelx51x:8080";
     return endpoint;
     
 }
