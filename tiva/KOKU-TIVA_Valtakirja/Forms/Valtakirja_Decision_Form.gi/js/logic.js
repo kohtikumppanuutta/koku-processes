ae9da2249aaaf1a21f1855c5cdceae491aff5d32
Kari Pöllänen <kari.pollanen@arcusys.fi>
2011-11-17 15:14:35 +0000

    Fixed getDomainName() and getUrl() functions

--- tiva/KOKU-TIVA_Valtakirja/Forms/Valtakirja_Decision_Form.gi/js/logic.js
+++ tiva/KOKU-TIVA_Valtakirja/Forms/Valtakirja_Decision_Form.gi/js/logic.js
@@ -11,7 +11,7 @@ function getEndpoint() {
 function getUrl() {
     
     var domain = getDomainName();
-    return "http://" + domain + "/palvelut-portlet/ajaxforms/WsProxyServlet2";
+    return domain + "/palvelut-portlet/ajaxforms/WsProxyServlet2";
 
 }
 
@@ -19,7 +19,7 @@ function getDomainName() {
 
     var url = window.location.href;
     var url_parts = url.split("/");
-    var domain_name = url_parts[2];
+    var domain_name = url_parts[0] + "//" + url_parts[2];
        
     return domain_name;
 
