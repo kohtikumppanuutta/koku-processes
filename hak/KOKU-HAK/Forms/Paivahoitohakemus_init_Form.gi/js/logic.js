0eaf272098ccbda8206b5f717dd798a8d94a1503
Henri Lämsä <henri.lamsa@arcusys.fi>
2011-11-23 08:03:55 +0000

    Acceptance testing

--- hak/KOKU-HAK/Forms/Paivahoitohakemus_init_Form.gi/js/logic.js
+++ hak/KOKU-HAK/Forms/Paivahoitohakemus_init_Form.gi/js/logic.js
@@ -7,8 +7,8 @@ function intalioPreStart() {
 
 function getEndpoint() {
     
-    var endpoint = "http://localhost:8180";
-    //var endpoint = "http://trelx51x:8080";
+    //var endpoint = "http://localhost:8180";
+    var endpoint = "http://trelx51x:8080";
     return endpoint;
     
 }
