d0688473ee9003a4d437f85a9f1cfb5dfd101b58
Henri Lämsä <henri.lamsa@arcusys.fi>
2011-12-13 09:53:25 +0000

    KOKU-860, group search with scandics letters fixed

--- kv/KOKU-Pyynto/form1_1.gi/js/logic.js
+++ kv/KOKU-Pyynto/form1_1.gi/js/logic.js
@@ -17,8 +17,8 @@ jsx3.lang.Package.definePackage(
 
 function getEndpoint() {
     
-    //var endpoint = "http://localhost:8180";
-    var endpoint = "http://trelx51lb:8080";
+    var endpoint = "http://localhost:8180";
+    //var endpoint = "http://trelx51lb:8080";
     return endpoint;
     
 }
@@ -2100,7 +2100,7 @@ jsx3.lang.Package.definePackage("Arcusys.Internal.Communication", function(arc) 
 
         req.open('POST', url, false);
 
-        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
         req.send(msg, tout);
         objXML = req.getResponseXML();
 
