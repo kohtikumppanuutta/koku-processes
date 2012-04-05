ff35f9d437eb52c9b65b707911d10f68560a6f18
Henri <henri.lamsa@arcusys.fi>
2012-04-05 10:43:01 +0300

    Fixed username showing incorrectly in Ajanvaraus sending form

--- av/KOKU-Ajanvaraus/Forms/Ajanvaraus_Init_Form.gi/js/logic.js
+++ av/KOKU-Ajanvaraus/Forms/Ajanvaraus_Init_Form.gi/js/logic.js
@@ -965,10 +965,15 @@ function mapFormDataToFields(objXML) {
  * Functions that has to run before form starts.
  */
 function Preload() {
-    var username, formData, id;
+    var username, formData, id, uid;
     username = Intalio.Internal.Utilities.getUser();
     username = username.substring(username.indexOf("/") + 1);
-    AjanvarausForm.getJSXByName("User_Sender").setValue(username).repaint();
+    uidData = Arcusys.Internal.Communication.GetUserUidByLooraname(username);
+    uid = uidData.selectSingleNode("//userUid", "xmlns:ns2='http://soa.common.koku.arcusys.fi/'").getValue();
+    userRealName = getUserRealName(uid);
+    if (userRealName) {
+    	AjanvarausForm.getJSXByName("User_Sender").setValue(userRealName).repaint();
+    }
     AjanvarausForm.getJSXByName("block").setHeight(0, true);
     if(gup("FormID")) {
         id = gup("FormID");
@@ -986,6 +991,18 @@ function Preload() {
         } catch (e) {
             alert(e);
         }
+    }
+}
+
+function getUserRealName(uid) {
+	var userData, firstname, lastname;
+    userData = Arcusys.Internal.Communication.getUserInfo(uid);
+    if (userData.selectSingleNode("//firstname", "xmlns:ns2='http://soa.av.koku.arcusys.fi/'") && userData.selectSingleNode("//lastname", "xmlns:ns2='http://soa.av.koku.arcusys.fi/'")) {
+        firstname = userData.selectSingleNode("//firstname", "xmlns:ns2='http://soa.av.koku.arcusys.fi/'").getValue();
+        lastname = userData.selectSingleNode("//lastname", "xmlns:ns2='http://soa.av.koku.arcusys.fi/'").getValue();
+        return firstname + " " + lastname;
+    } else {
+    	return null;
     }
 }
 
@@ -1573,6 +1590,39 @@ jsx3.lang.Package.definePackage("Arcusys.Internal.Communication", function(arc) 
 
     };
 });
+
+jsx3.lang.Package.definePackage("Arcusys.Internal.Communication", function(arc) {
+    arc.GetUserUidByLooraname = function(username) {
+
+        var tout = 1000;
+        var limit = 100;
+        var searchString = "";
+
+        var msg = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:soa=\"http://soa.common.koku.arcusys.fi/\"><soapenv:Header/><soapenv:Body><soa:getUserUidByLooraName><looraUsername>" + username + "</looraUsername></soa:getUserUidByLooraName></soapenv:Body></soapenv:Envelope>";
+
+        var url = getUrl();
+
+        endpoint = getEndpoint("UsersAndGroupsService");
+        // var endpoint = getEndpoint() + "/arcusys-koku-0.1-SNAPSHOT-arcusys-common-0.1-SNAPSHOT/UsersAndGroupsServiceImpl";
+        msg = "message=" + encodeURIComponent(msg) + "&endpoint=" + encodeURIComponent(endpoint);
+
+        var req = new jsx3.net.Request();
+
+        req.open('POST', url, false);
+
+        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        req.send(msg, tout);
+        var objXML = req.getResponseXML();
+
+        if(objXML == null) {
+            alert("Virhe palvelinyhteydess\xE4");
+        } else {
+            return objXML;
+
+        }
+    };
+});
+
 // Extra Functions -------------------------------------------------------------------------------------------------------------------------------
 
 //Getting the domain name and port if available
